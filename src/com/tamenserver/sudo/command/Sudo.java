945c4ec227ddf9a2def9c1e358614a41de71dc4c
TaMenServer <TaMenServer@gmail.com>
2016-12-17 13:44:44 +0800

    Update

--- src/com/tamenserver/sudo/command/Sudo.java
+++ src/com/tamenserver/sudo/command/Sudo.java
@@ -1,13 +1,31 @@
 package com.tamenserver.sudo.command;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
 
 public class Sudo extends SudoCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // TODO Auto-generated method stub
+        if(args.length<3){
+            sender.sendMessage("参数太少！");
+            
+        }else{
+            StringBuilder sb = new StringBuilder();
+            for(int i=2;i<args.length;i++){
+                sb.append(args[i]);
+            }
+            try{
+                if(args[1].equalsIgnoreCase("Console")){
+                    plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), sb.toString());
+                }else{
+                    
+                }
+            }catch (CommandException e){
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
