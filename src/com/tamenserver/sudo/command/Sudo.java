def1d480a5a4319f394490fef7f52029dd8a01ec
TaMenServer <TaMenServer@gmail.com>
2016-12-17 13:46:19 +0800

    Update

--- src/com/tamenserver/sudo/command/Sudo.java
+++ src/com/tamenserver/sudo/command/Sudo.java
@@ -3,14 +3,14 @@ package com.tamenserver.sudo.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Sudo extends SudoCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length<3){
-            sender.sendMessage("参数太少！");
-            
+            sender.sendMessage("参数太少！");          
         }else{
             StringBuilder sb = new StringBuilder();
             for(int i=2;i<args.length;i++){
@@ -20,10 +20,11 @@ public class Sudo extends SudoCommand {
                 if(args[1].equalsIgnoreCase("Console")){
                     plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), sb.toString());
                 }else{
-                    
+                    Player p = plugin.getServer().getPlayer(args[1]);
+                    plugin.getServer().dispatchCommand(p, sb.toString());
                 }
             }catch (CommandException e){
-                e.printStackTrace();
+                sender.sendMessage("强制指令执行出现错误!");
             }
         }
         return false;
